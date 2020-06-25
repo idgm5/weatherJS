10f0b25e70e9bf3112f6cd05bf6576d1fd3234dd
idgm5 <idgm@pm.me>
2020-06-25 16:02:22 -0500

    Add API request and save object in current localstorage

--- src/index.js
+++ src/index.js
@@ -2,33 +2,35 @@ import './style.css';
 import Form from './form';
 import Weather from './weather';
 import RequestWeather from './checkWeather';
-
 let currentCity = JSON.parse(localStorage.getItem('currentCity'));
-
 if (currentCity === null) {
   currentCity = [];
 }
 
 const checkCity = (city) => {
   const lastestCheck = new Weather(city, 0);
-  RequestWeather.checkWeather(city).then(response => {
+  RequestWeather.checkWeather(city).then((response) => {
     lastestCheck.temperature = response.main.temp;
-      console.log(lastestCheck);
   });
-  // currentCity.push(lastestCheck)
-  // return lastestCheck.save(currentCity);
+};
+
+let newcity = JSON.parse(localStorage.getItem('city'));
+
+if (newcity === null) {
+  newcity = '';
+} else {
+  checkCity(newcity);
 }
 
 Form.render();
+
 const form = document.getElementById('weather-form');
 const submit = document.createElement('button');
 submit.setAttribute('type', 'submit');
 submit.classList.add('form-button');
-submit.addEventListener("click", () => {
-  if(form[0].checkValidity){
-    checkCity(form[0].value);
-  }
-});
-submit.innerHTML = "Check Weather";
+submit.onclick = () => {
+  localStorage.setItem('city', JSON.stringify(form[0].value));
+};
 
+submit.innerHTML = 'Check Weather';
 form.appendChild(submit);
