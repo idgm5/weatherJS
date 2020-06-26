4a4e06243ff880475c1a01516c1ffb5f8e093d10
idgm5 <idgm@pm.me>
2020-06-26 15:35:25 -0500

    Finish CSS styling, add feels like, image interactive based on weather condition and rename city

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import RequestWeather from './checkWeather';
 
 const checkCity = (city) => {
   RequestWeather.checkWeather(city).then((response) => {
-    const lastestCheck = new Weather(city, response.main.temp);
+    localStorage.clear();
+    const lastestCheck = new Weather(response.name, response.main.temp, response.main.feels_like, response.weather[0].main);
     localStorage.setItem('currentCity', JSON.stringify(lastestCheck));
+    window.location.reload();
   });
 };
 
@@ -34,19 +36,33 @@ form.appendChild(submit);
 
 if (currentCity) {
   const image = document.createElement('img');
-  image.setAttribute('src', './assets/cloud.svg')
+  if(currentCity.condition === 'Rain'){
+    image.setAttribute('src', './assets/rain.svg');
+  }else if(currentCity.condition === 'Snow'){
+    image.setAttribute('src', './assets/snow.svg');
+  }else if(currentCity.condition === 'Clear'){
+    image.setAttribute('src', './assets/sun.svg');
+  }else{
+    image.setAttribute('src', './assets/cloud.svg');
+  }
   const tempDiv = document.createElement('div');
   const tempP = document.createElement('P');
+  const feelsP = document.createElement('P');
   const mainDiv = document.getElementById('form-background-div');
+  feelsP.classList.add('feels-like');
+
   tempP.innerHTML = `${Math.floor(currentCity.temperature)}°`;
+  feelsP.innerHTML = `Feels like ${Math.floor(currentCity.feelslike)}°`;
 
   const cityDiv = document.createElement('div');
+  const conditionP = document.createElement('P');
   const cityP = document.createElement('P');
   cityDiv.classList.add('city-name');
-  
-  cityP.innerHTML = `${currentCity.city}`;
 
-  tempDiv.appendChild(tempP);
-  cityDiv.appendChild(cityP);
+  conditionP.innerHTML = currentCity.condition;
+  cityP.innerHTML = currentCity.city;
+
+  tempDiv.append(tempP, feelsP);
+  cityDiv.append(conditionP, cityP);
   mainDiv.append(tempDiv, image, cityDiv);
 }
--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,9 @@
 export default class Weather {
-  constructor(city, temperature = 0) {
+  constructor(city, temperature = 0, feelslike, condition) {
     this.city = city;
     this.temperature = temperature;
+    this.feelslike = feelslike;
+    this.condition = condition;
   }
 
   save(lastcheck) {
